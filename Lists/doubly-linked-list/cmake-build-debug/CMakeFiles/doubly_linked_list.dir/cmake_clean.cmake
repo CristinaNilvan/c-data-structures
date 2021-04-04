23ab99217b2a3cc0e8d5def0c6987c56c0d04d8d
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-04-04 21:26:28 +0300

    Create doubly-linked-list

---
+++ Lists/doubly-linked-list/cmake-build-debug/CMakeFiles/doubly_linked_list.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doubly_linked_list.dir/main.c.obj"
+  "doubly_linked_list.exe"
+  "doubly_linked_list.exe.manifest"
+  "doubly_linked_list.pdb"
+  "libdoubly_linked_list.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/doubly_linked_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
